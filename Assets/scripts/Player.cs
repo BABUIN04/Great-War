6fcd3208001c22fb1f61b93093c46c95b2e67eef
svin2021 <128043138+svin2021@users.noreply.github.com>
2024-03-14 19:49:24 +0300

    NormalMovement, InterfaceInteract

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float Speed;
+    private Interactable interactableObject;
+    private void Update()
     {
-        
+        if (interactableObject != null && Input.GetKeyDown(KeyCode.E))
+        {
+            interactableObject.Interact();
+            Debug.Log("interact");
+        }
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        transform.Translate(movement * Speed * Time.fixedDeltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.tag == "interactable")
+        {
+            interactableObject = collision.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "interactable")
+        {
+            interactableObject = null;
+        }
     }
 }
